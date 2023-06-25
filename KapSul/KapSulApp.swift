a4d5cba6437d4a9168cf48dfb11ebcbe8982fd69
githubuiux <uiuxista@gmail.com>
2023-06-25 17:03:58 +0300

    Initial Commit

---
+++ KapSul/KapSulApp.swift
@@ -0,0 +1,17 @@
+//
+//  KapSulApp.swift
+//  KapSul
+//
+//  Created by Levent on 25.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct KapSulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
